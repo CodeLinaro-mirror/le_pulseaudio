244428597814c40dac7ac492f509e1c556404532
Arun Raghavan <arunsr@codeaurora.org>
2019-01-17 11:44:21 +0530

    format: Add support for compressed AAC data
    The assumption is that most decoders don't really care about differences
    between MPEG-2 and MPEG-4 AAC data. Clients will likely need to add some
    things to the pa_format_info proplist to make this usable:
    
      * MPEG-2 vs. MPEG-4 AAC (for decoders that care)
      * Stream format (raw vs. ADTS vs. LOAS)
      * AudioSpecificConfig to signal AudioObjectType and any other params

--- src/pulse/format.h
+++ src/pulse/format.h
@@ -76,6 +76,9 @@ typedef enum pa_encoding {
     PA_ENCODING_MPEG,
     /**< MPEG-1 or MPEG-2 (Part 3, not AAC) data. \since: FIXME */
 
+    PA_ENCODING_AAC,
+    /**< MPEG-2 or MPEG-4 AAC data. \since: FIXME */
+
     PA_ENCODING_MAX,
     /**< Valid encoding types must be less than this value */
 
@@ -96,6 +99,7 @@ typedef enum pa_encoding {
 #define PA_ENCODING_UNKNOWN_IEC61937 PA_ENCODING_UNKNOWN_IEC61937
 #define PA_ENCODING_UNKNOWN_4X_IEC61937 PA_ENCODING_UNKNOWN_4X_IEC61937
 #define PA_ENCODING_MPEG PA_ENCODING_MPEG
+#define PA_ENCODING_AAC PA_ENCODING_AAC
 #define PA_ENCODING_MAX PA_ENCODING_MAX
 #define PA_ENCODING_INVALID PA_ENCODING_INVALID
 /** \endcond */
