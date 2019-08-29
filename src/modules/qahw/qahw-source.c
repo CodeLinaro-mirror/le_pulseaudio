848731692f116b5eda6ead0c35c8666b6ae6993a
Lijo George <lgeorge@codeaurora.org>
2019-08-29 15:14:15 +0530

    qahw: Restore channel count if channel map is not changed
    - Multi channel clips are not played properly in offload path
    - Channel count of sink/source is not restored when qahw fails to
      open the requested channel configuration at HAL.
      Hence pulsecore won't do required resampling.
    - When qahw fails to open output/input with requested channel map,
      Channel count also should be restored to previous value along with the map.
    
    CRs-fixed: 2516771
    
    Change-Id: I236beb1dda5214a6b6bbcad7af8bc2e09ef2b7bf
    Signed-off-by: Lijo George <lgeorge@codeaurora.org>

--- src/modules/qahw/qahw-source.c
+++ src/modules/qahw/qahw-source.c
@@ -397,13 +397,17 @@ static int pa_qahw_source_reconfigure_cb(pa_source *s, pa_sample_spec *spec, pa_
 
             if (pa_sdata->avoid_config_processing & PA_QAHW_CARD_AVOID_PROCESSING_FOR_CHANNELS)
                 new_map = *map;
-            else
+            else {
                 new_map = pa_sdata->source->channel_map;
+                tmp_spec.channels = pa_sdata->source->sample_spec.channels;
+            }
         } else {
             if (pa_sdata->avoid_config_processing & PA_QAHW_CARD_AVOID_PROCESSING_FOR_CHANNELS)
                 pa_channel_map_init_auto(&new_map, tmp_spec.channels, PA_CHANNEL_MAP_DEFAULT);
-            else
+            else {
                 new_map = pa_sdata->source->channel_map;
+                tmp_spec.channels = pa_sdata->source->sample_spec.channels;
+            }
         }
 
         qahw_sdata->devices = *((audio_devices_t *)PA_DEVICE_PORT_DATA(pa_sdata->source->active_port));
