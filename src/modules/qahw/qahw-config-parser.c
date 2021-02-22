30eaec373827cd161f06dcfc9c6194cb36432fe2
Lijo George <lgeorge@codeaurora.org>
2021-02-22 14:08:09 +0530

    qahw: fix memory leak issue
    fix memory leak issues in qahw module while playing
    dolby/pcm over HDMI
    
    Change-Id: I6c7135fa7213081d58d071fd5c248d9cc86d5a20

--- src/modules/qahw/qahw-config-parser.c
+++ src/modules/qahw/qahw-config-parser.c
@@ -295,7 +295,7 @@ static int pa_qahw_config_parse_effect_endpoint_names(pa_config_parser_state *st
     effect->endpoint_conf_string =  pa_split_spaces_strv(state->rvalue);
     items = effect->endpoint_conf_string;
 
-    if (!(items = pa_split_spaces_strv(state->rvalue))) {
+    if (!items) {
         pa_log_error("%s: [%s:%u] port name missing", __func__, state->filename, state->lineno);
         goto exit;
     }
@@ -322,6 +322,7 @@ static int pa_qahw_config_parse_effect_endpoint_names(pa_config_parser_state *st
         pa_log_error("%s: invalid endpoint %s", __func__, endpoint_name);
         goto exit;
     }
+
     ret = 0;
 exit:
     return ret;
@@ -1981,6 +1982,9 @@ static void pa_qahw_config_free_port(pa_qahw_card_port_config *port) {
 
     pa_xfree(port->description);
 
+    if (port->primary_port_name)
+        pa_xfree(port->primary_port_name);
+
     if (port->port_type)
         pa_xfree(port->port_type);
 
@@ -2043,6 +2047,9 @@ static void pa_qahw_config_free_port(pa_qahw_card_port_config *port) {
 
     if (port->channel_status_path)
         pa_xfree(port->channel_status_path);
+
+    if (port->bus)
+        pa_xfree(port->bus);
 
     pa_idxset_free(port->formats, (pa_free_cb_t) pa_format_info_free);
 
