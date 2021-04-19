c6ea59d94819533f390faac3e50c74faa4e20f01
Harendra Gautam <hgautam@codeaurora.org>
2021-04-19 19:13:59 +0530

    qahw: Handle read and stop concurrency.
    - Read is getting stuck which is causing PA main
      thread block during standby call.
    - Race condition between read and stop call
      causing execution of stop before read starts
      in HAL, due to that read can not be stopped
      if it gets stuck.
    - Add mutex lock to handle the concurrency between
      read and stop call.
    
    Change-Id: I54d6c3c80b597e0707195baed372522d94dec760

--- src/modules/qahw/qahw-source.c
+++ src/modules/qahw/qahw-source.c
@@ -24,6 +24,8 @@
 #include <stdio.h>
 #include <errno.h>
 #include <unistd.h>
+#include <time.h>
+#include <pthread.h>
 
 #include <pulse/rtclock.h>
 #include <pulse/timeval.h>
@@ -58,6 +60,10 @@
 #define PA_DEFAULT_STARTUP_LATENCY_USEC (200 * 1000)
 #define PA_A2DP_STARTUP_LATENCY_USEC (500 * 1000)
 #define PA_A2DP_RUNTIME_DELAY_USEC (200 * 1000)
+
+#define PA_QAHW_STOP_WAIT_TIMEOUT_SEC   0
+#define PA_QAHW_STOP_WAIT_TIMEOUT_MSEC  150
+#define PA_QAHW_STOP_WAIT_TIMEOUT_NSEC  (PA_QAHW_STOP_WAIT_TIMEOUT_MSEC * 1000000)
 
 //#define SOURCE_DUMP_ENABLED
 
@@ -106,6 +112,8 @@ typedef struct {
     pa_thread_mq  qahw_thread_mq;
     pa_rtpoll *qahw_thread_rtpoll;
     qahw_msg_obj *qahw_msg;
+
+    pthread_mutex_t qahw_source_mutex;
 
     trace_log ts_log;
 } qahw_source_data;
@@ -683,6 +691,7 @@ static void qahw_source_thread_func(void *userdata) {
         if (qahw_sdata->flags & QAHW_INPUT_FLAG_TIMESTAMP)
             in_buf.timestamp = (int64_t *)&chunk.timestamp;
 
+        pthread_mutex_lock(&qahw_sdata->qahw_source_mutex);
         if (!pa_atomic_load(&qahw_sdata->stopped)) {
             if ((ret = qahw_in_read(qahw_sdata->in_handle, &in_buf)) <= 0) {
                 pa_log_error("qahw_in_read failed, ret = %d, qahw handle %p, sleeping for %" PRIu64 "ms",
@@ -730,6 +739,7 @@ static void qahw_source_thread_func(void *userdata) {
         pa_asyncmsgq_post(pa_sdata->thread_mq.inq, PA_MSGOBJECT(pa_sdata->source), PA_QAHW_SOURCE_READ_EVENT_DONE, NULL, 0, &chunk,NULL);
 
 poll:
+        pthread_mutex_unlock(&qahw_sdata->qahw_source_mutex);
         if ((ret = pa_rtpoll_run(qahw_sdata->qahw_thread_rtpoll, wait)) < 0)
             goto fail;
 
@@ -1311,11 +1321,27 @@ exit:
 
 static int stop_qahw_source(qahw_source_data *qahw_sdata) {
     int rc;
+    bool timer_expired = false;
+    struct timespec timeout_info;
 
     pa_assert(qahw_sdata);
 
+    pa_log_debug("%s", __func__);
+
+    clock_gettime(CLOCK_REALTIME, &timeout_info);
+    timeout_info.tv_sec += PA_QAHW_STOP_WAIT_TIMEOUT_SEC;
+    timeout_info.tv_nsec += PA_QAHW_STOP_WAIT_TIMEOUT_NSEC;
+
+    if (pthread_mutex_timedlock(&qahw_sdata->qahw_source_mutex, &timeout_info)) {
+        timer_expired = true;
+        pa_log_info("%s: stop wait timer expired", __func__);
+    }
+
     pa_atomic_store(&qahw_sdata->stopped, 1);
     rc = qahw_in_stop(qahw_sdata->in_handle);
+
+    if (!timer_expired)
+        pthread_mutex_unlock(&qahw_sdata->qahw_source_mutex);
 
     return rc;
 }
