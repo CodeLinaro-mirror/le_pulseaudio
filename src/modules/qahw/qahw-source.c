36fceccc87156b4a67a88bc7ca69c7f560b0fce2
Sandhya Mutha Naga Venkata <quic_smuthana@quicinc.com>
2023-05-19 18:20:34 +0530

    pulseaudio: fix crash during qahw source/sink reconfigure
    - qahw msg object free in close qahw source/sink causes crash
      during source/sink reconfigure
    
    - free qahw msg object in free qahw source instead of close
      qahw source
    
    Change-Id: I666207e10733c72d654648de0c5cc07353aefcdf

--- src/modules/qahw/qahw-source.c
+++ src/modules/qahw/qahw-source.c
@@ -2,7 +2,7 @@
  * Copyright (c) 2018-2020, The Linux Foundation. All rights reserved.
  *
  * Changes from Qualcomm Innovation Center are provided under the following license:
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023, Qualcomm Innovation Center, Inc. All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause-Clear
  *
  * This library is free software; you can redistribute it and/or modify
@@ -940,9 +940,6 @@ static int close_qahw_source(qahw_source_data *qahw_sdata) {
         pa_log_debug("%s, Ack closing qahw source rc: %d", __func__, rc);
     }
 
-    if (qahw_sdata->qahw_msg)
-        pa_xfree(qahw_sdata->qahw_msg);
-
 #ifdef SOURCE_DUMP_ENABLED
     close(qahw_sdata->write_fd);
 #endif
@@ -1008,6 +1005,9 @@ static void free_qahw_source_thread_resources(qahw_source_data *qahw_sdata) {
 
     if (qahw_sdata->qahw_thread_rtpoll)
         pa_rtpoll_free(qahw_sdata->qahw_thread_rtpoll);
+
+    if (qahw_sdata->qahw_msg)
+        pa_xfree(qahw_sdata->qahw_msg);
 
 }
 
