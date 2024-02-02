4a188060083add03c864dbf31e422650e917fd8c
Puneeth Prabhu <quic_pprabh@quicinc.com>
2024-02-02 13:03:24 +0530

    pal: Support native sample rate for headset
    -Update sink conf in conf file.
    -Update table for supported sample rates.
    -Remove assert for stream-handle before stream open.
    
    Change-Id: I69617e38385ba8633212e8b3113a1acb6b6372fb

--- src/modules/qal/module-qal-card/qal-sink.c
+++ src/modules/qal/module-qal-card/qal-sink.c
@@ -75,7 +75,7 @@ static int open_pal_sink(pa_pal_sink_data *sdata);
 static int pa_pal_set_param(pal_sink_data *pal_sdata, uint32_t param_id);
 
 static const uint32_t supported_sink_rates[] =
-                          {8000, 11025, 16000, 22050, 44100, 48000, 96000, 192000};
+                          {8000, 11025, 16000, 22050, 32000, 44100, 48000, 88200, 96000, 176400, 192000, 352800, 384000};
 
 static const char *pa_pal_sink_get_name_from_type(pal_stream_type_t type) {
     const char *name = NULL;
